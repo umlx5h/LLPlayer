968ad480049829eb3c8b8a3f6942c56364370e97
umlx5h <umlx5h21@protonmail.com>
2025-01-20 13:46:44 +0900

    Flyleaf: Set ValueConversion attribute to Converters

--- FlyleafLib/Controls/WPF/Converters.cs
+++ FlyleafLib/Controls/WPF/Converters.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.Globalization;
-using System.Windows;
+﻿using System.Globalization;
 using System.Windows.Data;
 
 namespace FlyleafLib.Controls.WPF;
 
+[ValueConversion(typeof(long), typeof(TimeSpan))]
 public class TicksToTimeSpanConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     => new TimeSpan((long)value);
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => ((TimeSpan)value).Ticks;
 }
 
+[ValueConversion(typeof(long), typeof(string))]
 public class TicksToTimeConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     => new TimeSpan((long)value).ToString(@"hh\:mm\:ss");
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
 
+[ValueConversion(typeof(long), typeof(double))]
 public class TicksToSecondsConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     => (long)value / 10000000.0;
@@ -24,6 +25,7 @@ public class TicksToSecondsConverter : IValueConverter
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (long)((double)value * 10000000);
 }
 
+[ValueConversion(typeof(long), typeof(int))]
 public class TicksToMilliSecondsConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     => (int)((long)value / 10000);
@@ -31,16 +33,10 @@ public class TicksToMilliSecondsConverter : IValueConverter
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => long.Parse(value.ToString()) * 10000;
 }
 
+[ValueConversion(typeof(AspectRatio), typeof(string))]
 public class StringToRationalConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     => value.ToString();
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => new AspectRatio(value.ToString());
-}
-
-public class BooleanToVisibilityConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     => (bool)value ? Visibility.Visible : Visibility.Collapsed;
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
