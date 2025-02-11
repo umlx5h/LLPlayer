6bc34c551a92e621150b527b450625bf28bbf38c
umlx5h <umlx5h21@protonmail.com>
2025-02-11 20:26:31 +0900

    fix(build): allow to publish by specifying architecture

---
+++ LLPlayer/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// For some reason, if these are not imported, it cannot be built with anything other than portable for target runtime.
+// Prism import errors occur, so work around this.
+global using Prism;
+global using Prism.Commands;
+global using Prism.DryIoc;
+global using Prism.Dialogs;
+global using Prism.Ioc;
