e8e989ffcbbe82dda2d4bfcf0060c0c2e61d3c8a
umlx5h <umlx5h21@protonmail.com>
2025-02-11 16:01:48 +0900

    fix(ASR): do not use WhisperFactory.GetSupportedLanguages, instead manually mage lang codes
    When WhisperFactory.GetSupportedLanguages is executed, the whisper runtime is automatically loaded.
    As a result, an unintended runtime was selected.
    
    so delay this until the actual ASR is executed. This should fix the bug that prevented the ASR settings from being opened.

--- FlyleafLib/Engine/WhisperLanguage.cs
+++ FlyleafLib/Engine/WhisperLanguage.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Whisper.net;
 
 namespace FlyleafLib;
 
@@ -17,9 +16,117 @@ public class WhisperLanguage
         return lang.Code == Code;
     }
 
+    // can get by calling WhisperFactory.GetSupportedLanguages()
+    // calling the above method will load the whisper runtime implicitly,
+    // As a result, unintended runtime may be used, so temporarily manually manage language codes
+    // ref: https://github.com/ggerganov/whisper.cpp/blob/d682e150908e10caa4c15883c633d7902d385237/src/whisper.cpp#L248-L348
+    private static List<string> WhisperLanguageCodes =
+    [
+        "en",
+        "zh",
+        "de",
+        "es",
+        "ru",
+        "ko",
+        "fr",
+        "ja",
+        "pt",
+        "tr",
+        "pl",
+        "ca",
+        "nl",
+        "ar",
+        "sv",
+        "it",
+        "id",
+        "hi",
+        "fi",
+        "vi",
+        "he",
+        "uk",
+        "el",
+        "ms",
+        "cs",
+        "ro",
+        "da",
+        "hu",
+        "ta",
+        "no",
+        "th",
+        "ur",
+        "hr",
+        "bg",
+        "lt",
+        "la",
+        "mi",
+        "ml",
+        "cy",
+        "sk",
+        "te",
+        "fa",
+        "lv",
+        "bn",
+        "sr",
+        "az",
+        "sl",
+        "kn",
+        "et",
+        "mk",
+        "br",
+        "eu",
+        "is",
+        "hy",
+        "ne",
+        "mn",
+        "bs",
+        "kk",
+        "sq",
+        "sw",
+        "gl",
+        "mr",
+        "pa",
+        "si",
+        "km",
+        "sn",
+        "yo",
+        "so",
+        "af",
+        "oc",
+        "ka",
+        "be",
+        "tg",
+        "sd",
+        "gu",
+        "am",
+        "yi",
+        "lo",
+        "uz",
+        "fo",
+        "ht",
+        "ps",
+        "tk",
+        "nn",
+        "mt",
+        "sa",
+        "lb",
+        "my",
+        "bo",
+        "tl",
+        "mg",
+        "as",
+        "tt",
+        "haw",
+        "ln",
+        "ha",
+        "ba",
+        "jw",
+        "su"
+    ];
+
     public static List<WhisperLanguage> GetWhisperLanguages()
     {
-        List<WhisperLanguage> whisperLangs = WhisperFactory.GetSupportedLanguages()
+        //List<WhisperLanguage> whisperLangs = WhisperFactory.GetSupportedLanguages()
+        List<WhisperLanguage> whisperLangs = WhisperLanguageCodes
             .Select(code =>
             {
                 var lang = Language.Get(code);
