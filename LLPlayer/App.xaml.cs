964df1a95eae17a9372070a8a3594a93a3446d23
umlx5h <umlx5h21@protonmail.com>
2025-05-15 13:45:28 +0900

    feat(YoutubeDL): add option of whether to automatically open automatic subtitles

--- LLPlayer/App.xaml.cs
+++ LLPlayer/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Threading;
